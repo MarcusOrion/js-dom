64f2fd12d037d5faf6e2d2e86ef437e59a29b1a8
MarcusOrion <manvati.marco@gmail.com>
2026-08-24 17:28:09 +0200

    add JS logic behind form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 /* Scrivere un programma che chieda all'utente:
     Il numero di chilometri da percorrere
     Età del passeggero */
+//DOM References
+const nameInput = document.getElementById('name');
+const distanceInput = document.getElementById('distance');
+const ageSelect = document.getElementById('age');
+//Evento click bottone
+const userForm = document.getElementById('user-form');
+userForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log('Andata a segno');
+//Collezione dati
+const name = nameInput.value;
+const distance = Numb(distanceInput.value);
+const age = Numb(ageSelect.value);
+console.log(name, distance, age)
+})
