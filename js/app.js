9971519a06be47f90523881eb051d7300afd6e92
MarcusOrion <manvati.marco@gmail.com>
2026-08-25 16:11:56 +0200

    add get information and print it

--- js/app.js
+++ js/app.js
@@ -5,6 +5,12 @@
 const nameInput = document.getElementById('name');
 const distanceInput = document.getElementById('distance');
 const ageSelect = document.getElementById('age');
+
+const displayName = document.getElementById('display-name');
+const displayOffer = document.getElementById('display-offer');
+const displayWagon = document.getElementById('display-wagon');
+const displayCode = document.getElementById('display-cp');
+const displayPrice = document.getElementById('display-cost');
 //Evento click bottone
 const userForm = document.getElementById('user-form');
 userForm.addEventListener('submit', function(event) {
@@ -20,16 +26,24 @@ let price = distance * 0.21;
 let discount = 0;
 //Applicazione sconti
 if(age === 'minorenne') {
-    discount = price * 0.80
+    discount = price * 0.20;
+    displayOffer.innerHTML = 'Biglietto Minorenne';
 } else if (age === 'over') {
-    discount  = price * 0.60
+    discount  = price * 0.40;
+    displayOffer.innerHTML = 'Biglietto Over 65';
+} else {
+    displayOffer.innerHTML = 'Biglietto Standard';
 }
 const finalPrice = price - discount;
 
 console.log(`Il prezzo del biglietto è ${finalPrice.toFixed(2)} €`);
+//Calcolo carrozza (randomico)
+let carrozza = Math.floor(Math.random()*10 + 1);
+//Calcolo Codice CP (randomico)
+let codice = Math.floor(Math.random() * 90000) + 10000;
+//Popolamento card Tuo Biglietto
+displayName.innerHTML = name;
+displayWagon.innerHTML = carrozza;
+displayCode.innerHTML = codice;
+displayPrice.innerHTML = finalPrice.toFixed(2) + '€';
 })
-
-/* Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
-    il prezzo del biglietto è definito in base ai km (0.21 € al km)
-    va applicato uno sconto del 20% per i minorenni
-    va applicato uno sconto del 40% per gli over 65. */
